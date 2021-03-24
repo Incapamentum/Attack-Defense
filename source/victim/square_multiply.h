df106319e349ecf57c298975854b3fceace622eb
Incapamentum <diazg.gustavoa@gmail.com>
2021-03-23 21:08:46 -0400

    Victim directory now contains a basic script.

--- source/victim/square_multiply.h
+++
@@ -1,8 +0,0 @@
-#ifndef __SQUAREMULTIPLY_H
-#define __SQUAREMULTIPLY_H
-
-int number_of_bits(unsigned int n);
-
-unsigned int square_multiply(unsigned int b, unsigned int e, unsigned int m, int num_bits);
-
-#endif
--- source/victim/victim.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-#include "square_multiply.h"
-
-int main(void)
-{
-    printf("Result of square-multiply: %d\n", square_multiply(2, 5, 3, number_of_bits(5)));
-}
