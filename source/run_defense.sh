fcf33f8dcd5f0916f1407b879bbebf41a1ee3a3a
Incapamentum <diazg.gustavoa@gmail.com>
2021-04-19 23:08:50 -0400

    Added new run script. Updated attack script.

---
+++ source/run_defense.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+
+
+if [[ $3 -ne 2 ]]; then
+    echo "Illegal number of parameters!"
+    echo ""
+    echo "Usage: ./run_defense.sh [core] [event_list_path]"
+    echo "      core            - the core oon which the spy and victim processes will execute on"
+    echo "      event_list_path - path to the conf file containing PAPI events to monitor"
+    exit 2
+fi
+
